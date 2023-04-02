f2c6758027fecd58f32f6f00500b236d36d30e56
InnPatron <randomcivvy0121@gmail.com>
2023-04-02 12:05:25 -0700

    Main comments

--- bin/main.ml
+++ bin/main.ml
@@ -5,6 +5,7 @@ module IntSer = struct
     let serialize v = string_of_int v
 end
 
+(* Serialize with enclosing '[' ']' *)
 module StackImpl1 = MakeSerStack_v1(ListStack(struct type elt = int end))(IntSer)
 module Serializer1 = Serializer(StackImpl1)
 
@@ -14,13 +15,19 @@ let stack'' = StackImpl1.push stack' 21
 
 let () = print_endline (Serializer1.serialize stack'')
 
+(* Serialize with enclosing '{' '}' *)
 module StackImpl2 = MakeSerStack_v2(ListStack(struct type elt = int end))(IntSer)
 module Serializer2 = Serializer(StackImpl2)
 let stack_v2 = StackImpl2.push (StackImpl2.push (StackImpl2.mkEmpty()) 0) 9000
 
 let () = print_endline (Serializer2.serialize stack_v2)
 
-module SoS = ListStack(struct type elt = StackImpl2.stack end)   (* stack of stacks *)
+(*
+ * stack of stacks example using alternating serialization schemes
+ * '[' and ']' for outer
+ * '{' and '}' for inner
+ *)
+module SoS = ListStack(struct type elt = StackImpl2.stack end)
 module StackImpl3 = MakeSerStack_v1(SoS)(Serializer2)
 module Serializer3 = Serializer(StackImpl3)
 
