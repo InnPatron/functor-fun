b7432e549eab190630caf70bc875f29aabcb469e
InnPatron <randomcivvy0121@gmail.com>
2023-04-02 12:41:30 -0700

    Top-level functor

--- bin/main.ml
+++ bin/main.ml
@@ -11,18 +11,18 @@ module IntStack = ListStack(struct type elt = int end)
 module StackImpl1 = MakeSerStack_v1(IntStack)(IntSerializer)
 module Serializer1 = Serializer(StackImpl1)
 
-let stack : StackImpl1.t = StackImpl1.mkEmpty()
-let stack' = StackImpl1.push stack 1337
-let stack'' = StackImpl1.push stack' 21
+let stack_v1 : StackImpl1.t = StackImpl1.mkEmpty()
+let stack_v1' = StackImpl1.push stack_v1 1337
+let stack_v1'' = StackImpl1.push stack_v1' 21
 
-let () = print_endline (Serializer1.serialize stack'')
+let () = print_endline (String.cat "v1 = " (Serializer1.serialize stack_v1''))
 
 (* Serialize with enclosing '{' '}' *)
 module StackImpl2 = MakeSerStack_v2(IntStack)(IntSerializer)
 module Serializer2 = Serializer(StackImpl2)
 let stack_v2 = StackImpl2.push (StackImpl2.push (StackImpl2.mkEmpty()) 0) 9000
 
-let () = print_endline (Serializer2.serialize stack_v2)
+let () = print_endline (String.cat "v2 = " (Serializer2.serialize stack_v2))
 
 (*
  * stack of stacks example using alternating serialization schemes
@@ -38,7 +38,7 @@ module Serializer3 = Serializer(StackImpl3)
 let stack_v3 = SoS.mkEmpty()
 let stack_v3' = SoS.push (SoS.push stack_v3 stack_v2) (let (s, _) = StackImpl2.pop stack_v2 in s)
 
-let () = print_endline (Serializer3.serialize stack_v3')
+let () = print_endline (String.cat "v3 = " (Serializer3.serialize stack_v3'))
 
 module SoS_Other = ListStack(struct type elt = StackImpl2.stack end)
 let stack_other = SoS_Other.mkEmpty()
@@ -47,3 +47,25 @@ let stack_other = SoS_Other.mkEmpty()
  * let () = print_endline (Serializer3.serialize stack_other)
  * let () = print_endline (StackImpl3.serialize stack_other)
  *)
+
+module MakeExecutor(Inner: SerializableStack with type elt = int) = struct
+    module InnerSerializer = Serializer(Inner)
+
+    module SoS = ListStack(struct type elt = Inner.stack end)
+    module StackImpl = MakeSerStack_v1(SoS)(InnerSerializer)
+    module OuterSerializer = Serializer(StackImpl)
+
+    let inner = Inner.push (Inner.push (Inner.mkEmpty()) 0) 9000
+    let stack = SoS.mkEmpty()
+    let stack' = SoS.push (SoS.push stack inner) (let (s, _) = Inner.pop inner in s)
+
+    let result = OuterSerializer.serialize stack'
+end
+
+(* Results like stack_v3', but uses '[' and ']' everywhere *)
+module Executor_v4 = MakeExecutor(StackImpl1)
+let () =  print_endline (String.cat "v4 = " Executor_v4.result)
+
+(* Results identical to stack_v3 *)
+module Executor_v5 = MakeExecutor(StackImpl2)
+let () =  print_endline (String.cat "v5 = " Executor_v5.result)
